427d9f8af2b5eb5521257d5e5a284ecc63cccac5
Warszawski19 <michal.tl.krakow@gmail.com>
2022-01-05 14:56:38 +0100

    class 11'

--- Class 11/After Class/zadanie_15.py
+++ Class 11/After Class/zadanie_15.py
@@ -4,5 +4,26 @@ class Point():
     def __init__(self,x,y):
         self.x = x
         self.y = y
+
     def __str__(self):
         return f'P({self.x},{self.y})'
+
+    def __eq__(self, other):
+        if self.x == other.x and self.y == other.y:
+            return "The distance beetwen points is 0"
+        else:
+            x = (self.x - other.x)
+            y = abs(self.y - other.y)
+            return (x**2 + y**2)**1/2
+
+p1 = Point(1,2)
+p2 = Point(1,2)
+p3 = Point(3,4)
+p4 = Point(4,5)
+
+print(p1)
+print(p2)
+print(p3)
+print(p4)
+print(p1 == p2)
+print(p3 == p4)
